41f89a8b61dc31fbcb2b7c1e8dacea56ec4496bc
William-figure <wyc19981230@outlook.com>
2024-07-18 17:49:09 +1000

    add an error modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,14 +41,45 @@ const Query = selection => {
       //     alert("please enter different search content!")
       //     return 0;
       // }
-
       if (searchBands.value) {
         searchInput = searchBands.value;
         localStorage.setItem("searchValue", searchBands.value);
       } else {
-        alert("please enter content to search!");
+        // alert("please enter content to search!");
+        resultsTable.innerHTML += `
+        <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+          <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <div class="sm:flex sm:items-start">
+                  <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                    </svg>
+                  </div>
+                  <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Error!</h3>
+                    <div class="mt-2">
+                      <p class="text-sm text-gray-500">You can't search for artist or songs without any text input!</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                  <button type="button" id="cancel-button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `
+        const buttonCancel = document.querySelector('#cancel-button');
+        buttonCancel.addEventListener("click", () => resultsTable.innerHTML = "")
         return 0;
       }
+
+    
 
       const URL = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
         searchInput
@@ -232,11 +263,11 @@ const handleRadioInputs = event => {
   });
 };
 
-window.onload = () => {
-  resultsTable.innerHTML = `<p class="text-slate-500 italic">Please search for results and select desired type.</p>` ;
-  // searchBands.value = localStorage.getItem("searchValue");
-  // resultsTable.innerHTML = JSON.parse(localStorage.getItem("recent-input"));
-};
+// window.onload = () => {
+//   // resultsTable.innerHTML = `<p class="text-slate-500 italic">Please search for results and select desired type.</p>` ;
+//   // searchBands.value = localStorage.getItem("searchValue");
+//   // resultsTable.innerHTML = JSON.parse(localStorage.getItem("recent-input"));
+// };
 submitBtn.addEventListener("click", handleRadioInputs);
 //end of the radio selection
 
